6805e5d9b8c70cbbfbc293ccaf72e67fa577d01b
andyrogers1973 <andy.rogers@digital.justice.gov.uk>
2024-03-19 13:45:48 +0000

    Adds queries to the script

--- bin/support_request_data.py
+++ bin/support_request_data.py
@@ -3,34 +3,39 @@ import pandas as pd
 from datetime import date
 from datetime import timedelta
 
-# Create dataframe
-# Set the max number of rows
+# Create dataframe from csv and set max rows
+df = pd.read_csv('csv/support_requests_mar24.csv')
 pd.options.display.max_rows = 9999
 
-# Create dataframe from csv, set first column as the index
-df = pd.read_csv('csv/support_requests_mar24.csv', index_col='Request Type')
-
-# print (f'dataframe = {df}')
-
-# sr = pd.DataFrame(df)       #convert series into a Dataframe - attempt to fix 'No axis named 2024-03-12 for object type dataframe' error
-# print(df.columns.values) returns columns labelled with '2024-03-12' etc (for whole month)
-
+# Set the index to Request Type column
+df.set_index(['Request Type'])
 
 # Define yesterdays date
 today = date.today()
 yesterday = today - timedelta(days=1)
 
-st_yesterday = str(yesterday)
+# Convert date to string
+str_yesterday = str(yesterday)
 
-# print(st_yesterday)
+# Count requests in column that matches yesterdays date and print the result
+total = df[str_yesterday].sum()
+print(f'Yesterday we received {total} Support Requests')
 
-# print (f'Yesterdays date = {yesterday}')
+# Create mask to remove NaN fields from 'str_yesterday' (none) and return Request Types and amount
+notna_msk = df[str_yesterday].notna()
+cols = ['Request Type', str_yesterday]
+sr_types = df.loc[notna_msk, cols]
 
-# print(df[['2024-03-11']])
+print(f'This is a breakdown of yesterdays Support Requests: \n {sr_types}')
 
-output = df[st_yesterday].sum()
 
-print(f'Yesterday we recieved {output} Support Requests')
+
+
+
+
+
+
+
 
 # print(yesterday)        # = 2024-03-11
 
@@ -59,3 +64,20 @@ print(f'Yesterday we recieved {output} Support Requests')
 
 # Show dataframe
 # print(df.to_string())
+
+# print (f'dataframe = {df}')
+
+# sr = pd.DataFrame(df)       #convert series into a Dataframe - attempt to fix 'No axis named 2024-03-12 for object type dataframe' error
+# print(df.columns.values) returns columns labelled with '2024-03-12' etc (for whole month)
+
+# print(st_yesterday)
+
+# print (f'Yesterdays date = {yesterday}')
+
+# print(df[['2024-03-11']])
+
+#for col in df.columns:
+#    print(col)
+
+# Print 2 columns to test working
+#print(df[['Request Type',str_yesterday]].to_string(index=False))
